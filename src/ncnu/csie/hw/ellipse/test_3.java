744b11f96b09d499d93801cfcd411cea03abb60c
rosechiu <rose103321016@gmail.com>
2016-07-25 23:32:31 +0800

    Add files via upload

---
+++ src/ncnu/csie/hw/ellipse/test_3.java
@@ -0,0 +1,13 @@
+package ncnu.csie.hw.ellipse;
+
+public class test_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ellipse e3 = new ellipse();
+		e3.setAB(20,18);
+		System.out.println("E3的面積 :"+e3.area());
+		System.out.println("E3的周長 :"+e3.perimeter());
+	}
+
+}
