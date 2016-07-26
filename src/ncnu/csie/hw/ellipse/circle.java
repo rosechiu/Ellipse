afe6f34619988aa21e621398266c5aa387d73886
rosechiu <rose103321016@gmail.com>
2016-07-26 17:52:56 +0800

    Add files via upload

---
+++ src/ncnu/csie/hw/ellipse/circle.java
@@ -0,0 +1,12 @@
+package ncnu.csie.hw.ellipse;
+
+public class circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Round r1 = new Round(10);
+		System.out.println("The area of this Circle is " + r1.area() + " cm^.");
+		System.out.println("The perimeter of this Circle is " + r1.perimeter() + " cm.");
+	}
+
+}
